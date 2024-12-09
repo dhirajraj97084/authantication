caa1be5b87082b879eae5543fd6d5f3450c1cfc4
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-12-09 23:49:51 +0530

    okay

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import Home from './pages/home/Home'
 import Login from './pages/registraction/Login'
 import Signup from './pages/registraction/Signup'
+import { ProtectedRoute } from './protectedRoute/ProtectedRoute'
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>} />
           <Route path='/login' element={<Login/>} />
           <Route path='/signup' element={<Signup/>} />
         </Routes>
